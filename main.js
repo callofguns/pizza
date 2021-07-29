d35b6cd7a038528f0f5feac263668ebe57fde054
callofguns <84049001+callofguns@users.noreply.github.com>
2021-07-29 12:42:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+menu_list_array = ["Veg Margherita Pizza","Chicken Tandoori Pizza","Veg Supreme Pizza","Paneer Tikka Pizza","Deluxe Veggie Pizza","Veg Extravaganza Pizza"];
+
+function getmenu(){
+var htmldata;
+htmldata = "<ol class='menulist'>"
+menu_list_array.sort();
+for (var i = 0;i<menu_list_array.length;i++){
+    htmldata = htmldata+'<li>' + menu_list_array[i] + '</li>'
+}
+htmldata = htmldata + "</ol>"
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
